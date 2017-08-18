1b93303e030aa4d35caf453d1c1a375db1675cf6
Malcom <gtshilombowanticale@adneom.com>
2017-08-18 16:16:19 +0200

    add interface to communicate between notification and map

---
+++ app/src/main/java/adneom/poc_widget/interfaces/ISnapHotGoogleMap.java
@@ -0,0 +1,12 @@
+package adneom.poc_widget.interfaces;
+
+import android.graphics.Bitmap;
+
+/**
+ * Created by gtshilombowanticale on 18-08-17.
+ */
+
+public interface ISnapHotGoogleMap {
+
+    public void senSnapshot(Bitmap bitmap);
+}
