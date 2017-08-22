6897744bc4a2ddd905e881273fb04b26ce7b7ff4
Malcom <gtshilombowanticale@adneom.com>
2017-08-22 11:59:45 +0200

    add interface to communicate with the view

---
+++ app/src/main/java/adneom/poc_widget/interfaces/IRequest.java
@@ -0,0 +1,11 @@
+package adneom.poc_widget.interfaces;
+
+
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.ArrayList;
+
+public interface IRequest {
+
+    public void notif(ArrayList<LatLng> points);
+}
